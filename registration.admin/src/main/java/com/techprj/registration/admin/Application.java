f548d96dc14c4a2a286089d970c47b047bda1aea
DesignedByQ <henry.o@hotmail.co.uk>
2023-06-23 15:53:38 +0100

    initial push

--- registration.admin/src/main/java/com/techprj/registration/admin/Application.java
+++ registration.admin/src/main/java/com/techprj/registration/admin/Application.java
@@ -2,12 +2,18 @@ package com.techprj.registration.admin;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
+	}
+	
+	@Bean
+	public Object ModelMapper() {
+		return ModelMapper;
 	}
 
 }
