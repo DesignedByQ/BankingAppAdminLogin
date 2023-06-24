d305d04772585e61ce550f318022df91539871d3
DesignedByQ <henry.o@hotmail.co.uk>
2023-06-24 11:56:51 +0100

    dto and entity setup and tables created

---
+++ registration.admin/src/main/java/com/techprj/registration/admin/api/ControllerAPI.java
@@ -0,0 +1,16 @@
+package com.techprj.registration.admin.api;
+
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@CrossOrigin
+@RequestMapping("/api")
+@Validated
+public class ControllerAPI {
+	
+	
+
+}
---
+++ registration.admin/src/main/java/com/techprj/registration/admin/dto/AdminDTO.java
@@ -0,0 +1,114 @@
+package com.techprj.registration.admin.dto;
+
+import java.time.LocalDate;
+
+public class AdminDTO {
+	
+	private Long idAdminDTO;
+	private AuthUserDTO authUserDTO;
+	private String firstName;
+	private String middleName;
+	private String lastName;
+	private Long mobile;
+	private String email;
+	private AddressDTO addressDTO;
+	private LocalDate staffSince;
+	
+	public AdminDTO() {
+		super();
+	}
+
+	public AdminDTO(Long idAdminDTO, AuthUserDTO authUserDTO, String firstName, String middleName, String lastName,
+			Long mobile, String email, AddressDTO addressDTO, LocalDate staffSince) {
+		super();
+		this.idAdminDTO = idAdminDTO;
+		this.authUserDTO = authUserDTO;
+		this.firstName = firstName;
+		this.middleName = middleName;
+		this.lastName = lastName;
+		this.mobile = mobile;
+		this.email = email;
+		this.addressDTO = addressDTO;
+		this.staffSince = staffSince;
+	}
+
+	public Long getIdAdminDTO() {
+		return idAdminDTO;
+	}
+
+	public void setIdAdminDTO(Long idAdminDTO) {
+		this.idAdminDTO = idAdminDTO;
+	}
+
+	public AuthUserDTO getAuthUserDTO() {
+		return authUserDTO;
+	}
+
+	public void setAuthUserDTO(AuthUserDTO authUserDTO) {
+		this.authUserDTO = authUserDTO;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getMiddleName() {
+		return middleName;
+	}
+
+	public void setMiddleName(String middleName) {
+		this.middleName = middleName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public Long getMobile() {
+		return mobile;
+	}
+
+	public void setMobile(Long mobile) {
+		this.mobile = mobile;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public AddressDTO getAddressDTO() {
+		return addressDTO;
+	}
+
+	public void setAddressDTO(AddressDTO addressDTO) {
+		this.addressDTO = addressDTO;
+	}
+
+	public LocalDate getStaffSince() {
+		return staffSince;
+	}
+
+	public void setStaffSince(LocalDate staffSince) {
+		this.staffSince = staffSince;
+	}
+
+	@Override
+	public String toString() {
+		return "AdminDTO [idAdminDTO=" + idAdminDTO + ", authUserDTO=" + authUserDTO + ", firstName=" + firstName
+				+ ", middleName=" + middleName + ", lastName=" + lastName + ", mobile=" + mobile + ", email=" + email
+				+ ", addressDTO=" + addressDTO + ", staffSince=" + staffSince + "]";
+	}
+
+}
